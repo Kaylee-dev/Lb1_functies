9a1fe21513fb59efe768f02e82e4936eafce6689
Kaylee <99057337@mydavinci.nl>
2020-10-27 08:33:09 +0100

    opdracht 1 af

--- opdracht1.js
+++ opdracht1.js
@@ -1,4 +1,5 @@
 function helloWorld(hello){ 
+
 	for(i=0; i< hello; i++){
 		document.write("Hello World!" + "<br>");
 	}
@@ -6,4 +7,4 @@ function helloWorld(hello){
  
 helloWorld(7);
 
- 
+ // De function heeft de naam helloWorld zodat je die makkelijk weer kan aanroepen. Het woord (hello) is voor de hoeveelheid en kan je terug vinden in de for loop. als variabel i =0 groter is dan hello doet hij wat er staat. En omdat ik de function 7 keer uit laat voeren staat er 7 keer Hello World!
