cccef68bd36e8d39e41b6405645ece6ecd30b932
Kaylee <99057337@mydavinci.nl>
2020-10-27 14:44:47 +0100

    comment toevoegen

--- opdracht4.js
+++ opdracht4.js
@@ -1,13 +1,13 @@
 function multiplication(){
 	var table = parseInt(prompt("Welke tafel wilt u uit rekenen?"));
 	// !isNaN(table) zorgt ervoor dat er word gekeken of table een number is. ! betekent niet (false).
-	//  Er word ook gekeken of tafel kleiner?gelijk aan 10 is en groter/gelijk aan 1.
+	//  Er word ook gekeken of tafel kleiner/gelijk aan 10 is en groter/gelijk aan 1.
 	if (!isNaN(table) && table <= 10 && table >= 1) {
 		for(i = 1; i <= table; i++){
 			for(t = 1; t <= 10; t++){
-				document.write(i + ' x ' + t + ' = ' + (i * t) + '<br />');
+				document.write(i + " x " + t + " = " + (i * t) + "<br>");
 			}
-		document.write('<br /><br />');
+		document.write("<br><br>");
 		}
 	} else {
 		if (isNaN(table)) {
